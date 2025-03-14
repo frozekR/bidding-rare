07c51deb1864ba0b024d8f440392acf6ce9ad20b
FrozekR <elkov.egor@gmail.com>
2025-03-14 20:08:43 +0300

    add shadcn components, update layouts, pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function HomePage() {
-  return <main className=""></main>
+import { Button } from "@/src/components/ui/button";
+import { database } from "../db/database";
+import { bids as bidsSchema } from "../db/schema";
+import { Input } from "../components/ui/input";
+
+export default async function HomePage() {
+  const bids = await database.query.bids.findMany();
+
+  return (
+  <main className="container mx-auto py-12">
+    <form
+    action={async (formData: FormData) => {
+      'use server';
+      // sconst bid = formData.get('bid') as string;
+      await database.insert(bidsSchema).values({});
+    }}>
+      <Input name="bid" placeholder="Bid" />
+      <Button type="submit">Place Bid</Button>
+    </form>
+
+    {bids.map(bid => (
+      <div key={bid.id}>{bid.id}</div>
+    ))}
+  </main>
+  )
 }
